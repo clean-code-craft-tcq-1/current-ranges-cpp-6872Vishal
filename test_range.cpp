6814671c6f31975c7715b5f9707ce71e1c0ba986
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-20 20:38:09 +0530

    Update test_range.cpp

--- test_range.cpp
+++ test_range.cpp
@@ -9,7 +9,7 @@ int main ()
 std::vector<StructRange*> AnalysedInfo;
 cout<<"************************************Test_Case_1******************************************"<<\n;
 int test_array[] ={3, 3, 4, 5, 10, 11, 12};
-int NumofTestArray = sizeof(test_array)/test_array[0];
+int NumofTestArray = sizeof(test_array)/sizeof(test_array[0]);
 AnalyseRange(test_array,NumofTestArray,AnalysedInfo);
 assert(AnalysedInfo.size()==2);
 }
