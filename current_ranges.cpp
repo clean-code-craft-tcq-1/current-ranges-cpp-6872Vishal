98274d5c10a41da2904ae68eb2e2d448d86e6693
6872Vishal <77484050+6872Vishal@users.noreply.github.com>
2021-05-20 21:18:13 +0530

    Try2

--- current_ranges.cpp
+++ current_ranges.cpp
@@ -1,36 +1,42 @@
 #include<iostream>
+#include <algorithm>
 #include"current_ranges.h"
-
 
 void PrinttheRange(int StartingRange,int EndingRange,int NoOfReads)
 {
-  cout<<StartingRange<<"-"<<EndingRange<<\t<<NoOfReads+1<<\n;
+  cout<<StartingRange<<"-"<<EndingRange<<"\t"<<NoOfReads<<"\n";
 }
 
 
 void AnalyseRange(const int* CurrentRange,int NoOfRanges,std::vector<StructRange*>& RangeInfo_st)
 {
   int StartRange, EndRange, NoOfReadings;
-  cout<<"/*----------------------------------------------*/"<<\n;
-  cout<<"Range"<<\t<<"Readings"<<\n;
+  cout<<"/*----------------------------------------------*/"<<"\n";
+  cout<<"Range"<<"\t"<<"Readings"<<"\n";
   
     StartRange = CurrentRange[0];
     EndRange   = CurrentRange[0];
-  for(i=0,i<NoOfRanges,i++)
+    NoOfReadings = 1;
+  for(int i=0;i<NoOfRanges;i++)
   {
     if((CurrentRange[i+1] - CurrentRange[i]) <= 1)
     {
       NoOfReadings++;
-      EndRange   = CurrentRange[i];
+      EndRange   = CurrentRange[i+1];
+      cout<<CurrentRange[i]<<"\n";
     }
     else
     {
       PrinttheRange(StartRange,EndRange,NoOfReadings);
-      RangeInfo_st.push_back(new StructRange{StartRange,EndRange,NoOfReadings+1});
+      RangeInfo_st.push_back(new StructRange{StartRange,EndRange,NoOfReadings});
       StartRange = CurrentRange[i+1];
       EndRange   = CurrentRange[i+1];
       NoOfReadings = 0;
     }
   }
+    EndRange   = CurrentRange[NoOfRanges - 1];
+    PrinttheRange(StartRange,EndRange,NoOfReadings);
+    RangeInfo_st.push_back(new StructRange{StartRange,EndRange,NoOfReadings});
 }
+
 
